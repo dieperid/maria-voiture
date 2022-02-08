2dd38a4669d15d9742d5ab3717312091b117d061
Kevindaddy Avdylaj <pq34bsi@etmlnet.local>
2022-02-08 17:22:54 +0100

    Implement car physics

---
+++ documentation/carphysics/Car.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Physics
+{
+    class Car
+    {
+        private float _width;
+        private float _height;
+        private float _speed;
+        private float _frontWheeAngle;
+        private float _acceleration;
+        private float _heading;
+        private float _breaking;
+        private Vector2f _position;
+        private Vector2f _velocity;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="w"> Width </param>
+        /// <param name="h"> Height </param>
+        /// <param name="s"> Speed </param>
+        /// <param name="a"> Acceleration </param>
+        /// <param name="he"> Heading </param>
+        /// <param name="p"> Position </param>
+        public Car(float w, float h, float s, float a, float he, Vector2f p)
+        {
+            _width = h;
+            _height = w;
+            _speed = s;
+            _acceleration = a;
+            _heading = he;
+            _position = p;
+            _breaking = 1;
+            _velocity = new Vector2f(1, 0);
+            _frontWheeAngle = 0;
+        }
+
+        public void Update(float dt)
+        {
+            _position += _velocity * dt;
+            _heading = (float)Math.Atan2(_velocity.Y, _velocity.X) * 180 / (float)Math.PI;
+        }
+
+        public void Accelerate(float dt)
+        {
+            Vector2f velunit = _velocity / (float)Math.Sqrt(_velocity.X * _velocity.X + _velocity.Y * _velocity.Y);
+            velunit *= _acceleration;
+            _velocity += velunit * dt;
+        }
+
+        public void Break(float dt)
+        {
+            Vector2f breaking = _velocity * -1;
+            _velocity += (breaking * _breaking) * dt;
+        }
+
+        public void Turn(bool left, float turnRate, float dt)
+        {
+            Vector2f leftvec = new Vector2f(_velocity.X * 0 + _velocity.Y * 1, _velocity.X * -1 + _velocity.Y * 0);
+            leftvec = leftvec / (float)Math.Sqrt(leftvec.X * leftvec.X + leftvec.Y * leftvec.Y);
+            Vector2f rightvec = leftvec * -1;
+            if (left)
+            {
+                _velocity += leftvec * turnRate * dt;
+            }
+            else
+            {
+                _velocity += rightvec * turnRate * dt;
+            }
+        }
+
+        public void Render(RenderWindow w)
+        {
+            RectangleShape rec = new RectangleShape(new Vector2f(_width,_height));
+            rec.FillColor = Color.Red;
+            rec.Position = _position;
+            rec.Origin = new Vector2f(_width / 2f, _height / 2f);
+            rec.Rotation = _heading;
+            w.Draw(rec);
+        }
+
+        public Vector2f Velocity
+        {
+            get { return _velocity; }
+            set { _velocity = value; }
+        }
+
+        public float Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public float Acceleration
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public float Heading
+        {
+            get { return _heading; }
+            set { _heading = value; }
+        }
+
+        public Vector2f Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+    }
+}
---
+++ documentation/carphysics/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using SFML.System;
+using SFML.Graphics;
+using SFML.Window;
+using SFML_Engine;
+using Physics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game(1920, 1080, "Car physics");
+            game.Run();
+            LogHandler.GetInstance().ShowLogs();
+        }
+    }
+
+    class CarPhysX : GameState
+    {
+        Car _myCar;
+        Label lbl;
+        Button btn;
+
+        public CarPhysX(string name, StateHandler sh, AssetManager am, InputHandler ih, GameClock gc, RenderWindow w, WidgetHandler wh) : base(name, sh, am, ih, gc, w, wh)
+        {
+        }
+
+        public override void Load()
+        {
+            _assetManager.Loadfiles(@"C:\Users\pq34bsi\Desktop\CarPhysics\Asset");
+            _widgetHandler.Font = _assetManager.GetFont("ayar.ttf");
+            _myCar = new Car(80, 150, 100, 100, 0, new Vector2f(1920f/2f, 1080f/2f));
+            lbl = _widgetHandler.CreateLabel();
+            lbl.Position = new Vector2f(0, 0);
+            btn = _widgetHandler.CreateButton();
+            btn.Color = Color.Red;
+            btn.Position = new Vector2f(0, 1030);
+            btn.Size = new Vector2f(300, 150);
+            btn.Text = "Reload";
+            btn.ClickedEvent += Btn_ClickedEvent;
+        }
+
+        private void Btn_ClickedEvent(object sender, EventArgs e)
+        {
+            _myCar = new Car(80, 150, 100, 100, 0, new Vector2f(1920f / 2f, 1080f / 2f));
+        }
+
+        public override void Update()
+        {
+            float turnRate = 500;
+            if (_inputHandler.IsPressed(Keyboard.Key.W))
+            {
+                _myCar.Accelerate(_gameClock.ElapsedFrame());
+            }
+            if (_inputHandler.IsPressed(Keyboard.Key.A))
+            {
+                _myCar.Turn(true, turnRate, _gameClock.ElapsedFrame());
+            }
+            if (_inputHandler.IsPressed(Keyboard.Key.D))
+            {
+                _myCar.Turn(false, turnRate, _gameClock.ElapsedFrame());
+            }
+            if (_inputHandler.IsPressed(Keyboard.Key.Space))
+            {
+                _myCar.Break(_gameClock.ElapsedFrame());
+            }
+            _myCar.Update(_gameClock.ElapsedFrame());
+            lbl.Text = $"Heading : {_myCar.Heading * 180 / (float)Math.PI:0.0}\n" +
+                       $"Velocity : X:{_myCar.Velocity.X:0.0} Y: {_myCar.Velocity.Y:0.0}\n" +
+                       $"Speed : {(float)Math.Sqrt(_myCar.Velocity.X * _myCar.Velocity.X + _myCar.Velocity.Y * _myCar.Velocity.Y):0} Pixels/Seconds\n" +
+                       $"Position : X:{_myCar.Position.X:0.0} Y: {_myCar.Position.Y:0.0}";
+        }
+
+        public override void Render()
+        {
+            _myCar.Render(_window);
+        }
+
+    }
+}
