71af20a5c76dee2524533b5575972e4da7780733
Alexander Gaillard <aproject37@gmail.com>
2022-02-08 16:27:31 +0100

    Menu version 1

---
+++ main-menu/main-menu/main-menu/Button.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace main_menu
+{
+    internal class Button : Widget
+    {
+        bool _oldState = false; // Old button's state
+        bool _state = false; // Actual button's state
+        Text _text; //Button's text
+        Texture _texutre; // Button's texture
+        public event EventHandler ClickedEvent; //Clicked event
+        public event EventHandler PressedEvent; //Pressed event
+        public event EventHandler ReleasedEvent; //Released event
+
+
+        public Button(Font f, InputHandler ih) : base(ih)
+        {
+            _text = new Text("", f);
+        }
+
+        public override void Update(float dt)
+        {
+            //Get mouse position
+            int mouseX = _inputHandler.GetMousePosition(true).X;
+            int mouseY = _inputHandler.GetMousePosition(true).Y;
+
+            //Change button state if clicked
+            if (mouseX >= _position.X && mouseX <= _position.X + _size.X && mouseY >= _position.Y && mouseY <= _position.Y + _size.Y)
+            {
+                if (_inputHandler.IsPressed(Mouse.Button.Left))
+                {
+                    _state = true;
+                }
+                else
+                {
+                    _state = false;
+                }
+            }
+
+            //Trigger event when button is clicked
+            if(!_oldState && _state)
+            {
+                ClickedEvent?.Invoke(this, EventArgs.Empty);
+            }
+            //Trigger event when button is released
+            else if(_oldState && !_state)
+            {
+                ReleasedEvent?.Invoke(this, EventArgs.Empty);
+            }
+            //Trigger event when button is pressed
+            if(_state)
+            {
+                PressedEvent?.Invoke(this, EventArgs.Empty);
+            }
+
+            //Update old button state
+            _oldState = _state;
+        }
+
+        public override void Render(RenderWindow w)
+        {
+            RectangleShape shape = new RectangleShape(_size);
+            shape.Position = _position;
+            _text.Position = _position;
+            shape.FillColor = _color;
+            shape.Texture = _texutre;
+            w.Draw(shape);
+            w.Draw(_text);
+        }
+
+        /// <summary>
+        /// Get/Set text
+        /// </summary>
+        public string Text
+        {
+            get { return _text.DisplayedString; }
+            set { _text.DisplayedString = value; }
+        }
+
+        public Texture Texture
+        {
+            get { return _texutre; }
+            set { _texutre = value; }
+        }
+    }
+}
---
+++ main-menu/main-menu/main-menu/MainMenu.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace main_menu
+{
+    class MainMenu : GameState
+    {
+        /// <summary>
+        /// Initialisation du bouton multijoueur
+        /// </summary>
+        Button btnMultiplayer;
+
+        /// <summary>
+        /// Initialisation du bouton solo
+        /// </summary>
+        Button btnSolo;
+
+        /// <summary>
+        /// Initialisation du bouton Option
+        /// </summary>
+        Button btnOption;
+
+        /// <summary>
+        /// Initialisation du bouton quit
+        /// </summary>
+        Button btnQuit;
+
+        /// <summary>
+        /// Initialisation d'un rectangleshape qui vas contenir le titre
+        /// </summary>
+        RectangleShape _title;
+
+        /// <summary>
+        /// Initialisation de la renderwindow
+        /// </summary>
+        RenderWindow _w;
+
+        /// <summary>
+        /// Initialisation d'un rectangleshape qui vas contenir le background du menu
+        /// </summary>
+        RectangleShape _background;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">Nom</param>
+        /// <param name="sh">StateHandler</param>
+        /// <param name="am">AssetManager</param>
+        /// <param name="ih">InputHandler</param>
+        /// <param name="gc">GameClock</param>
+        /// <param name="w">RenderWindow</param>
+        /// <param name="wh">WidgetHandler</param>
+        public MainMenu(string name, StateHandler sh, AssetManager am, InputHandler ih, GameClock gc, RenderWindow w, WidgetHandler wh) : base(name, sh, am, ih, gc, w, wh)
+        {
+            _w = w;
+        }
+
+        #region[Méthode lorsque on appuie sur un bouton]
+
+        /// <summary>
+        /// Méthode qui vas etre effectuer lorsqu'on appuie sur le bouton Multijoueur
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="args">EventArgs</param>
+        public void btnMultiplayerClick(object sender, EventArgs args)
+        {
+            //_stateHandler.SetNextState(typeof(MenuMulti).Name);
+        }
+        /// <summary>
+        /// Méthode qui vas etre effectuer lorsqu'on appuie sur le bouton Solo
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="args">EventArgs</param>
+        public void btnSoloClick(object sender, EventArgs args)
+        {
+            //_stateHandler.SetNextState(typeof(MenuSolo).Name);
+        }
+        /// <summary>
+        /// Méthode qui vas etre effectuer lorsqu'on appuie sur le bouton Option
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="args">EventArgs</param>
+        public void btnOptionClick(object sender, EventArgs args)
+        {
+            //_stateHandler.SetNextState(typeof(MenuOption).Name);
+        }
+        /// <summary>
+        /// Méthode qui vas etre effectuer lorsqu'on appuie sur le bouton Quit
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="args">EventArgs</param>
+        public void btnQuitClick(object sender, EventArgs args)
+        {
+            Environment.Exit(0);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Méthode qui vas être lancer lors du lancement
+        /// </summary>
+        public override void Load()
+        {
+            // Création du rectangle contenant le background
+            _background = new RectangleShape(new Vector2f(_window.Size.X, _window.Size.Y));
+            _background.Texture = new Texture(Resource1.background);
+
+            // Création du rectangle contenant le titre
+            _title = new RectangleShape(new Vector2f(1277, 145)); // Image 1277 x 145
+            _title.Position = new Vector2f(270, 100);
+            _title.Texture = new Texture(Resource1.btnTitle);
+
+            // Création du bouton multijoueur
+            btnMultiplayer = _widgetHandler.CreateButton();
+            btnMultiplayer.Texture = new Texture(Resource1.btnMultiplayer); // Image 575 x 99
+            btnMultiplayer.Position = new Vector2f(158, 450);
+            btnMultiplayer.Size = new Vector2f(575, 99);
+            btnMultiplayer.ClickedEvent += btnMultiplayerClick;
+
+            //Création du bouton solo
+            btnSolo = _widgetHandler.CreateButton();
+            btnSolo.Texture = new Texture(Resource1.btnSolo); // Image 576 x 100
+            btnSolo.Position = new Vector2f(1118, 450);
+            btnSolo.Size = new Vector2f(576, 100);
+            btnSolo.ClickedEvent += btnSoloClick;
+
+            // Création du bouton options
+            btnOption = _widgetHandler.CreateButton();
+            btnOption.Texture = new Texture(Resource1.btnOption); // Image 575 x 101
+            btnOption.Position = new Vector2f(158, 650);
+            btnOption.Size = new Vector2f(575, 101);
+            btnOption.ClickedEvent += btnOptionClick;
+
+            // Création du bouton quit
+            btnQuit = _widgetHandler.CreateButton();
+            btnQuit.Texture = new Texture(Resource1.btnQuit); // Image 576 x 100
+            btnQuit.Position = new Vector2f(1118, 650);
+            btnQuit.Size = new Vector2f(576, 100);
+            btnQuit.ClickedEvent += btnQuitClick;
+        }
+
+        /// <summary>
+        /// Méthode ou on vas intégrer les updates
+        /// </summary>
+        public override void Update()
+        {
+            //if (_inputHandler.IsClicked(Keyboard.Key.A))
+            //{
+            //    _stateHandler.SetNextState(typeof(Main2Menu).Name);
+            //}
+        }
+
+        /// <summary>
+        /// Méthode ou on vas vouloir afficher quelque chose
+        /// </summary>
+        public override void Render()
+        {
+            // Affichage du background toujour en premier
+            _w.Draw(_background);
+            // Affichage des autres inputs
+            _w.Draw(_title);
+            
+        }
+    }
+}
