f3f94582cd5a0f61abfa4cfdc8fc79dd4d43c56b
Aurélien Dévaud <pp50oip@eduvaud.ch>
2022-03-01 17:22:09 +0100

    map 2 in progress 2

--- racetrack/recetrack-implementation/racetrack-implementation/racetrack-implementation/Wall2.cs
+++ racetrack/recetrack-implementation/racetrack-implementation/racetrack-implementation/Wall2.cs
@@ -60,6 +60,45 @@ namespace racetrack_implementation
             CreateNewWall(20, 10, 330, 210);
             CreateNewWall(10, 10, 350, 200);
             CreateNewWall(30, 10, 360, 190);
+            CreateNewWall(50, 10, 390, 180);
+            CreateNewWall(20, 10, 440, 170);
+            CreateNewWall(60, 10, 460, 160);
+            CreateNewWall(110, 10, 520, 150);
+
+            CreateNewWall(10, 40, 220, 410);
+            CreateNewWall(10, 60, 210, 450);
+            CreateNewWall(10, 70, 200, 510);
+            CreateNewWall(10, 10, 210, 580);
+            CreateNewWall(10, 10, 220, 590);
+            CreateNewWall(20, 10, 230, 600);
+            CreateNewWall(40, 10, 250, 610);
+            CreateNewWall(10, 10, 290, 620);
+            CreateNewWall(10, 10, 300, 630);
+            CreateNewWall(20, 10, 310, 640);
+            CreateNewWall(30, 10, 330, 650);
+            CreateNewWall(30, 10, 360, 660);
+            CreateNewWall(10, 10, 390, 670);
+            CreateNewWall(40, 10, 400, 680);
+            CreateNewWall(10, 10, 440, 690);
+            CreateNewWall(10, 20, 450, 700);
+            CreateNewWall(10, 10, 460, 720);
+            CreateNewWall(10, 10, 470, 730);
+            CreateNewWall(10, 10, 480, 740);
+            CreateNewWall(10, 10, 490, 750);
+            CreateNewWall(10, 50, 500, 760);
+            CreateNewWall(150, 10, 510, 800);
+            CreateNewWall(40, 10, 660, 810);
+            CreateNewWall(40, 10, 700, 820);
+            CreateNewWall(10, 10, 740, 830);
+            CreateNewWall(190, 10, 750, 840);
+            CreateNewWall(280, 10, 940, 850);
+            CreateNewWall(60, 10, 1220, 840);
+            CreateNewWall(50, 10, 1280, 830);
+            CreateNewWall(40, 10, 1330, 820);
+            CreateNewWall(30, 10, 1370, 810);
+            CreateNewWall(40, 10, 1400, 800);
+            CreateNewWall(40, 10, 1440, 790);
+            CreateNewWall(60, 10, 1480, 780);
         }
     }
 }
