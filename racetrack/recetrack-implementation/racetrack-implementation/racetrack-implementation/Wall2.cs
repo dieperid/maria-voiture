b3993812f47873d67ca306570a19f480acbaa7f4
Aurélien Dévaud <pp50oip@eduvaud.ch>
2022-03-01 16:26:31 +0100

    map 2 in progress

---
+++ racetrack/recetrack-implementation/racetrack-implementation/racetrack-implementation/Wall2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace racetrack_implementation
+{
+    public partial class Wall
+    {
+        public void CreateAllWallsMap2()
+        {
+            CreateNewWall(10, 10, 1050, 220);
+            CreateNewWall(10, 10, 1040, 230);
+            CreateNewWall(10, 60, 1030, 240);
+            CreateNewWall(10, 50, 1020, 300);
+            CreateNewWall(10, 30, 1010, 350);
+            CreateNewWall(10, 40, 1000, 380);
+            CreateNewWall(10, 20, 990, 420);
+            CreateNewWall(10, 20, 980, 440);
+            CreateNewWall(10, 20, 970, 460);
+            CreateNewWall(10, 30, 960, 480);
+            CreateNewWall(10, 10, 950, 510);
+            CreateNewWall(10, 10, 940, 520);
+            CreateNewWall(10, 10, 930, 530);
+            CreateNewWall(10, 10, 920, 540);
+            CreateNewWall(10, 10, 910, 550);
+            CreateNewWall(10, 20, 900, 560);
+            CreateNewWall(10, 10, 890, 580);
+            CreateNewWall(20, 10, 870, 590);
+            CreateNewWall(20, 10, 850, 600);
+            CreateNewWall(20, 10, 830, 610);
+            CreateNewWall(100, 10, 730, 620);
+            CreateNewWall(150, 10, 580, 610);
+            CreateNewWall(30, 10, 550, 600);
+            CreateNewWall(60, 10, 490, 590);
+            CreateNewWall(30, 10, 460, 580);
+            CreateNewWall(40, 10, 420, 570);
+            CreateNewWall(40, 10, 380, 560);
+            CreateNewWall(10, 10, 370, 550);
+            CreateNewWall(20, 10, 350, 540);
+            CreateNewWall(20, 10, 330, 530);
+            CreateNewWall(10, 10, 320, 520);
+            CreateNewWall(10, 10, 310, 510);
+            CreateNewWall(10, 10, 300, 500);
+            CreateNewWall(10, 10, 290, 490);
+            CreateNewWall(10, 10, 280, 480);
+            CreateNewWall(10, 10, 270, 470);
+            CreateNewWall(10, 10, 260, 460);
+            CreateNewWall(10, 10, 250, 450);
+            CreateNewWall(10, 40, 240, 410);
+            CreateNewWall(10, 50, 230, 360);
+            CreateNewWall(10, 30, 240, 330);
+            CreateNewWall(10, 30, 250, 300);
+            CreateNewWall(10, 10, 260, 290);
+            CreateNewWall(10, 20, 270, 270);
+            CreateNewWall(10, 10, 280, 260);
+            CreateNewWall(10, 10, 290, 250);
+            CreateNewWall(10, 10, 300, 240);
+            CreateNewWall(10, 10, 310, 230);
+            CreateNewWall(10, 10, 320, 220);
+            CreateNewWall(20, 10, 330, 210);
+            CreateNewWall(10, 10, 350, 200);
+            CreateNewWall(30, 10, 360, 190);
+        }
+    }
+}
